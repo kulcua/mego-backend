32349f20b929c4676fc18f834e73438f0c79226f
kulcua <17520834@gm.uit.edu.vn>
2020-12-14 23:41:34 +0700

    upload image google drive

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Str;
 // use Illuminate\Support\Facades\File; 
 use Illuminate\Support\Facades\Storage;
 
+// $googleDriveStorage = Storage::disk('google');
+// $dirBanner = '/1XSaFHzV4lFrx-mGR3GmtWTwxmPLauZXn';
+// $recursive = false;
+// $contents = collect($googleDriveStorage->listContents($dirBanner, $recursive));
+
 class FileController extends Controller
 {
     public function getAllDataProductImg()
@@ -21,120 +26,122 @@ class FileController extends Controller
         return response()->json(BannerImageModel::orderByRaw('priority ASC')->get(), 200);
     }
 
-    public function getImgByName($fileName)
-    {
-        return response()->download(public_path('img/'.$fileName));
-    }
-
     public function getImgByProductDetailId($product_detail_id)
     {
-        //can get only 1 image with 1 product_detail_id
-        $list_image = ProductImageModel::where('product_detail_id', $product_detail_id)->get();
-        return response()->download(public_path('img/'.$list_image->first()->name));
+        $image = ProductImageModel::where('product_detail_id', $product_detail_id)->first();
+
+        if (empty(json_decode($image, true))) {
+            return response()->json(["message" => "ID Not Found"], 404);
+        }
+
+        $googleDriveStorage = Storage::disk('google');
+
+        $fileinfo = collect($googleDriveStorage->listContents('/', false))
+            ->where('name', $image->name)
+            ->first();
+
+        $contents = $googleDriveStorage->get($fileinfo['path']);
+
+        return response($contents)
+            ->header('Content-Type', $fileinfo['mimetype'])
+            ->header('Content-Disposition', "attachment; filename='" . $fileinfo['name'] . "'");
     }
 
     public function getImgByPriority($priority)
     {
         //can get only 1 image with 1 product_detail_id
-        $image = ProductImageModel::where('priority', $priority)->get();
-        return response()->download(public_path('img/'.$image->first()->name));
+        $image = BannerImageModel::where('priority', $priority)->first();
+
+        if (empty(json_decode($image, true))) {
+            return response()->json(["message" => "ID Not Found"], 404);
+        }
+
+        $googleDriveStorage = Storage::disk('google');
+
+        $fileinfo = collect($googleDriveStorage->listContents('/', false))
+            ->where('name', $image->name)
+            ->first();
+
+        $contents = $googleDriveStorage->get($fileinfo['path']);
+
+        return response($contents)
+            ->header('Content-Type', $fileinfo['mimetype'])
+            ->header('Content-Disposition', "attachment; filename='" . $fileinfo['name'] . "'");
     }
 
     public function uploadProductImg(Request $request)
     {
-        $fileName = Str::random(10).'.png';
+        //1. storage image in file system public/img/..
+        $path = $request->file('photo')->store('', 'google');
 
-        //1. save link image in database
+        //2. save link image in database
         $image = new ProductImageModel();
-        $image->name = $fileName;
+        $image->name = $path;
         $image->origin_name = $request->file('photo')->getClientOriginalName();
         $image->product_detail_id = $request->product_detail_id;
         $image->save();
 
-        //2. storage image in file system public/img/..
-        $path = $request->file('photo')->move(public_path('/img'), $fileName);
-        $photoURL = url('/'.$fileName);
-
-        return response()->json(['url: ', $photoURL], 200);
+        return response()->json(['upload success ' . $path], 200);
     }
 
     public function uploadBannerImg(Request $request)
     {
-        $fileName = Str::random(10).'.png';
+        //1. storage image in file system public/img/..
+        $path = $request->file('photo')->store('', 'google');
 
-        //1. save link image in database
+        //2. save link image in database
         $image = new BannerImageModel();
-        $image->name = $fileName;
+        $image->name = $path;
         $image->origin_name = $request->file('photo')->getClientOriginalName();
         $image->priority = $request->priority;
         $image->save();
 
-        //2. storage image in file system public/img/..
-        $path = $request->file('photo')->move(public_path('/img'), $fileName);
-        $photoURL = url('/'.$fileName);
-
-        return response()->json(['url: ', $photoURL], 200);
+        return response()->json(['upload success ' . $path], 200);
     }
 
     public function deleteProductImgById($id)
     {
         $image = ProductImageModel::find($id);
-        
-        if (is_null($image))
-        {
+
+        if (empty(json_decode($image, true))) {
             return response()->json(["message" => "ID Not Found"], 404);
         }
 
-        //delete file in folder
-        if(Storage::disk('local_public')->exists($image->name)){
-            Storage::disk('local_public')->delete($image->name);
-        }
+        $googleDriveStorage = Storage::disk('google');
 
-        //delete fil name in database
+        $fileinfo = collect($googleDriveStorage->listContents('/', false))
+            ->where('type', 'file')
+            ->where('name', $image->name)
+            ->first();
+        
+        $googleDriveStorage->delete($fileinfo['path']);
+
+        //delete file name in database
         $image->delete();
 
-        return response()->json(['Deleted'], 200);
-    }
-
-    public function deleteProductImgByProductDetail($product_detail_id)
-    {
-        $image = ProductImageModel::where('product_detail_id', $product_detail_id)->get();
-        
-        //check image in database
-        if (empty(json_decode($image, true)))
-        {
-            return response()->json(["message" => "ID Not Found"], 404);
-        }
-        
-        //delete file in folder
-        if(Storage::disk('local_public')->exists($image->name)){
-            Storage::disk('local_public')->delete($image->name);
-        }
-        else return response()->json(["message" => "Image Not Found"], 404);
-
-        //delete fil name in database
-        $image->delete();
-
-        return response()->json(['Deleted'], 200);
+        return response()->json(['Deleted image name: '.$image->name.' where id = '.$id], 200);
     }
 
     public function deleteBannerImgById($id)
     {
         $image = BannerImageModel::find($id);
-        
-        if (is_null($image))
-        {
+
+        if (empty(json_decode($image, true))) {
             return response()->json(["message" => "ID Not Found"], 404);
         }
 
-        //delete file in folder
-        if(Storage::disk('local_public')->exists($image->name)){
-            Storage::disk('local_public')->delete($image->name);
-        }
+        $googleDriveStorage = Storage::disk('google');
 
-        //delete fil name in database
+        $fileinfo = collect($googleDriveStorage->listContents('/', false))
+            ->where('type', 'file')
+            ->where('name', $image->name)
+            ->first();
+        
+        $googleDriveStorage->delete($fileinfo['path']);
+
+        //delete file name in database
         $image->delete();
 
-        return response()->json(['Deleted'], 200);
+        return response()->json(['Deleted banner image name: '.$image->name.' where id = '.$id], 200);
     }
 }
